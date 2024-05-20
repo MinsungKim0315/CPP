ad57c1d4a0f32f7c6d777b1e40fdf6c6a2813fe3
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-05-20 13:51:47 +0900

    Update Bank_ver2.cpp

--- Day03/Structure and Class/Bank_ver2.cpp
+++ Day03/Structure and Class/Bank_ver2.cpp
@@ -1,4 +1,4 @@
-//구조체 멤버 변수 ver.
+//구조체 멤버 변수 + 멤버 함수 ver.
 #include <iostream>
 using namespace std;
 struct ACT {
@@ -6,30 +6,27 @@ struct ACT {
     char pw[5];             //비밀번호
     char name[20];          //이름
     int bal;                //잔액
-void showData() {
-    cout << "\n계좌번호: " << ACTId << endl;
-    cout << name << " 회원님의 잔액은 " << bal << endl;
-}
-void deposit() {
-    int m;
-    cout << "\n입금금액: ";
-    cin >> m;
-    bal += m;
-    cout << "\t\t" << m << "원 입금완료\n";
-}
-void withdrawl() {
-    int m;
-    cout << "\n출금금액: ";
-    cin >> m;
-    bal -= m;
-    cout << "\t\t" << m << "원 출금완료\n";
-}
+    void showData() {
+        cout << "\n계좌번호 : " << ACTId << endl;
+        cout << name << " 회원님의 잔액은 " << bal << "입니다.\n";
+    }
+    void deposit() {
+        int m;
+        cout << "\n입금금액 입력 : ";
+        cin >> m;        bal += m;
+        cout << "\t\t" << m << "원 입금완료.\n";
+    }
+    void withdraw() {
+        int m;
+        cout << "\n출금금액 입력 : ";
+        cin >> m;        bal -= m;
+        cout << "\t\t" << m << "원 출금완료.\n";
+    }
 };
-int main() {
-    ACT act_1 = { 1234, "5679", "chris", 10000 };
-    act_1.showData();
-    act_1.deposit();
-    act_1.withdrawl();
-    act_1.showData();
-
+int main(){
+    ACT act_1 = { 1234, "5678", "chris", 10000 };
+    act_1.showData(); //계좌 정보 출력 멤버 함수 호출
+    act_1.deposit(); //입금 멤버 함수 호출
+    act_1.withdraw(); //출금 멤버 함수 호출
+    act_1.showData(); //계좌 정보 출력 멤버 함수 호출
 }
